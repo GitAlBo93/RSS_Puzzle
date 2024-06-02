cebceead9f6e3b04063b96d7ca90dbb6fa0e1da7
Alex Bog <a.l.e.k.s.93@mail.ru>
2024-06-02 10:42:02 +0500

    feat: на тестовом предложении произвел парсинг новых элементов по словам

--- src/app/view/game_page/game_view.ts
+++ src/app/view/game_page/game_view.ts
@@ -11,8 +11,23 @@ export default class GamePage extends ConstructorView {
         const Section = document.createElement('section');
         Section.className = 'sectionGame';
 
+        const SettingDiv = this.constructorDiv('div', 'settingDiv', 'settingDivID');
+        Section.appendChild(SettingDiv);
+
+            const  LevelDiv = this.constructorForm('div', 'levelDiv');
+            SettingDiv.appendChild(LevelDiv);
+
+            const  SetBtnDiv = this.constructorForm('div', 'setBtnDiv');
+            SettingDiv.appendChild(SetBtnDiv);
+
         const TaskDiv = this.constructorDiv('div', 'taskDiv', 'taskDivID');
         Section.appendChild(TaskDiv);
+
+            const SpeakTaskDiv = this.constructorDiv('div', 'speakTaskDiv', 'speakTaskDivID');
+            TaskDiv.appendChild(SpeakTaskDiv);
+
+            const SentTaskDiv = this.constructorDiv('div', 'sentTaskDiv', 'sentTaskDivID');
+            TaskDiv.appendChild(SentTaskDiv);
 
         const PuzzleDiv = this.constructorDiv('div', 'puzzleDiv', 'puzzleDivID');
         Section.appendChild(PuzzleDiv);
@@ -20,8 +35,23 @@ export default class GamePage extends ConstructorView {
         const SentenceDiv = this.constructorDiv('div', 'sentenceDiv', 'sentenceDivID');
         Section.appendChild(SentenceDiv);
 
-        const SettingDiv = this.constructorDiv('div', 'settingDiv', 'settingDivID');
-        Section.appendChild(SettingDiv);
+            const SentencePuzzleDiv = this.constructorDiv('div', 'sentPuzzleDiv', 'sentPuzzleDivID');
+            SentenceDiv.appendChild(SentencePuzzleDiv);
+
+            const BTNSentenceDiv = this.constructorDiv('div', 'BTNSentDiv', 'BTNSentDivID');
+            SentenceDiv.appendChild(BTNSentenceDiv);
+
+            const worlds = 'Они прибыли в школу в 7 часов утра';
+            let worldsSplit = worlds.split(' ');
+            console.log(worldsSplit);
+
+            worldsSplit.forEach((world) =>{
+            const testWorlds = this.constructorH1('h1', 'testWorldsClass', world);
+            SentenceDiv.appendChild(testWorlds);  
+            });
+
+
+        
 
         return Section;
     }
--- src/app/view/start_page/start_view.ts
+++ src/app/view/start_page/start_view.ts
@@ -16,7 +16,7 @@ export default class StartPage {
         // const name = new getFLName();
         // name.sendFLName();
         console.log(localStorage.getItem('Name'));
-        
+
         const Section = document.createElement('section');
         Section.className = 'sectionStart';
 
@@ -37,7 +37,8 @@ export default class StartPage {
         const Welcom = document.createElement('p');
         Welcom.className = 'welcom';
         // Welcom.textContent = 'Welcom in Puzzle ' + name.sendFLName();
-        Welcom.textContent = 'Welcom in Puzzle ' + localStorage.getItem('Name') + ' ' + localStorage.getItem('LastName');
+        Welcom.textContent =
+            'Welcom in Puzzle ' + localStorage.getItem('Name') + ' ' + localStorage.getItem('LastName');
         Container.appendChild(Welcom);
 
         const TextGame = document.createElement('p');
@@ -51,7 +52,7 @@ export default class StartPage {
         BtnWelcom.textContent = 'Game Start';
         Container.appendChild(BtnWelcom);
 
-        BtnWelcom.addEventListener('click', ()=> {
+        BtnWelcom.addEventListener('click', () => {
             window.location.hash = 'Game';
         });
 
@@ -60,12 +61,11 @@ export default class StartPage {
         BtnLogOff.textContent = 'Выход';
         Container.appendChild(BtnLogOff);
 
-        BtnLogOff.addEventListener('click', ()=> {
+        BtnLogOff.addEventListener('click', () => {
             localStorage.clear();
             window.location.hash = 'Login';
             // console.log(localStorage);
-            
-        })
+        });
 
         return Section;
     }
